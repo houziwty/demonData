7ae77f72110bdc4bde109268fb690e67a217daae
wangtianyu <wangtianyu@zhongwenweilai.com>
2016-08-22 19:09:18 +0800

    guava缓存没有完成

--- src/main/java/com/data/cache/GuavaCache.java
+++ src/main/java/com/data/cache/GuavaCache.java
@@ -3,15 +3,23 @@ package com.data.cache;
 import java.util.concurrent.TimeUnit;
 
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
 public class GuavaCache<K,V> {
+	
+	
 	public   V put(K key,V  value){
-		LoadingCache<key, value>graphs=
+		LoadingCache<K, V>graphs=
 		CacheBuilder.newBuilder().maximumSize(1000).expireAfterWrite(10, TimeUnit.MINUTES)
-//		.removalListener(MY)
+		.build(new CacheLoader<K, V>(){
+			@Override
+			public V load(K key) throws Exception {
+				return  (V) key;
+			}
+			
+		});
 		return value;
-
 	}
 
 }

bdb4fbfb130a5f54140dd2e9938aa2a9c4cc1b22
wangtianyu <wangtianyu@zhongwenweilai.com>
2016-08-30 11:51:37 +0800

    完成队列接口
    完成队列接口 但是实现还在构思中

--- src/main/java/com/data/queue/NativeQueueClient.java
+++ src/main/java/com/data/queue/NativeQueueClient.java
@@ -1,5 +1,85 @@
 package com.data.queue;
 
+import java.util.List;
+
 public class NativeQueueClient implements QueueClient {
+
+	@Override
+	public QueueClient getQueueDelayClient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QueueClient getQueueNormalClient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void enqueue(byte[] value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enqueueBatch(List<byte[]> values) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enqueuehead(byte[] value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<byte[]> dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] dequeueDelay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long llen() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delbyvalue(byte[] value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delqueue() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delay() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void quit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public byte[] lpop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/com/data/queue/QueueClient.java
+++ src/main/java/com/data/queue/QueueClient.java
@@ -1,5 +1,31 @@
 package com.data.queue;
 
-public interface QueueClient {
+import java.util.List;
 
+public interface QueueClient {
+	QueueClient getQueueDelayClient();
+
+	QueueClient getQueueNormalClient();
+
+	void enqueue(byte[] value);
+
+	void enqueueBatch(final List<byte[]> values);
+
+	void enqueuehead(byte[] value);// 从头部添加
+
+	List<byte[]> dequeue();
+
+	byte[] dequeueDelay();
+
+	Long llen();
+
+	void delbyvalue(byte[] value);
+
+	void delqueue();
+
+	void delay();
+
+	void quit();
+
+	byte[] lpop();
 }

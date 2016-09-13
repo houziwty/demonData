1d58107c89015ce74c412e0b37cf6b5e3b38b57f
王天宇 <wangtianyu@zhongwenweilai.com>
2016-09-13 17:29:29 +0800

    完善队列

--- src/main/java/com/data/queue/QueueListener.java
+++ src/main/java/com/data/queue/QueueListener.java
@@ -3,6 +3,9 @@ package com.data.queue;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class QueueListener implements Runnable {
 	public static interface TaskHandler {
 		void proccess(byte[] taskInfo);
@@ -20,10 +23,38 @@ public class QueueListener implements Runnable {
 
 		@Override
 		public void run() {
-
+			while (true) {
+				try {
+					byte[] taskInfo = queueClient.dequeueDelay();
+					if (taskInfo != null) {
+						try {
+							handler.proccess(taskInfo);
+						} catch (Exception e) {
+							LOGGER.error(e.toString(), e);
+						}
+					}else{
+					QueueClient client=queueClient.getQueueNormalClient();
+					long len=client.llen();
+					if(len>DELAY_THRESHOLD_DEFUALT){
+						client.delay();
+					}else{
+						Thread.sleep(SLEEP_MILLIS);
+					}
+					}
+				} catch (Exception e) {
+					try {
+						Thread.sleep(SLEEP_MILLIS);
+					} catch (Exception ex) {
+						LOGGER.error(ex.getMessage(), ex);
+					}
+					LOGGER.error(e.getMessage(), e);
+				}
+			}
 		}
 
 	}
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(QueueListener.class);
 
 	private QueueClient queueClient;
 	private TaskHandler handler;
@@ -64,6 +95,17 @@ public class QueueListener implements Runnable {
 
 	public void start() {
 		// 主线程也包含在内了，兼容已经引用此处代码的逻辑
+		for (int i = 0; i < threads - 1; i++) {
+			Thread thread = new Thread(new QueueListener(this.queueClient, this.handler, this.threads));
+			thread.setDaemon(true);
+			thread.start();
+		}
+		if (delayThread) {
+			Thread thread = new Thread(new QueueDelayListener(this.queueClient.getQueueDelayClient(), this.handler));
+			thread.setDaemon(true);
+			thread.start();
+		}
+		this.run();
 
 	}
 

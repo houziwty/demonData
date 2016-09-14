ece8d28e8835b0d56ba18047b7191eb8370f4574
王天宇 <wangtianyu@zhongwenweilai.com>
2016-09-14 17:27:00 +0800

    增加test

--- src/test/java/com/data/QueueTest.java
+++ src/test/java/com/data/QueueTest.java
@@ -1,10 +1,38 @@
 package com.data;
 
+import com.data.queue.NativeQueueClient;
+import com.data.queue.QueueListener;
+import com.data.queue.QueueListener.TaskHandler;
+
 public class QueueTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private class Test1 implements TaskHandler {
 
+		@Override
+		public void proccess(byte[] taskInfo) {
+			String str = taskInfo.toString();
+			System.out.println(str);
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("begin");
+		NativeQueueClient a = new NativeQueueClient("测试");
+		for (int i = 0; i < 10; i++) {
+			String aa = "测试" + i;
+			a.enqueue(aa.getBytes());
+		}
+		new QueueListener(a, new TaskHandler() {
+			@Override
+			public void proccess(byte[] taskInfo) {
+				String str = taskInfo.toString();
+				System.out.println(str);
+			}
+		}).start();
+
+		System.out.println("end");
 	}
 
 }

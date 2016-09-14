8f20d91d5507f7141b50b97f4e8e4f17a581fd81
王天宇 <wangtianyu@zhongwenweilai.com>
2016-09-14 14:40:50 +0800

    设计实现本地队列

--- src/main/java/com/data/queue/NativeQueueClient.java
+++ src/main/java/com/data/queue/NativeQueueClient.java
@@ -1,84 +1,86 @@
 package com.data.queue;
 
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class NativeQueueClient implements QueueClient {
 
+	private BlockingQueue<Object> queue;
+
+	private static final int DEFAULT_TIMEOUT = 5;
+	private static final int DEFAULT_COUNT = 10;
+
+	private String name;
+
+	public NativeQueueClient(String name) {
+		this.name = name;
+		queue = new ArrayBlockingQueue(DEFAULT_COUNT);
+	}
+
 	@Override
 	public QueueClient getQueueDelayClient() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public QueueClient getQueueNormalClient() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void enqueue(byte[] value) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void enqueueBatch(List<byte[]> values) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void enqueuehead(byte[] value) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public List<byte[]> dequeue() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public byte[] dequeueDelay() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Long llen() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void delbyvalue(byte[] value) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void delqueue() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void delay() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void quit() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public byte[] lpop() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 

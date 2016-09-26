239aa411761c39c88052c3d8f2167fd62ddd05aa
王天宇 <wangtianyu@zhongwenweilai.com>
2016-09-26 17:51:36 +0800

    完成datatable

--- src/main/java/com/data/database/DataRow.java
+++ src/main/java/com/data/database/DataRow.java
@@ -1,20 +1,33 @@
 package com.data.database;
 
-/** 
-* @ClassName: DataRow 
-* @Description:  * <b>描述：</b>数据行
- * <p>
- * <b>功能：</b>数据行的使用类
- * <p>
- * <b>用法：</b>
- * <p>  
- * 根据字段序号（从1开是的序号）或者字段名访问行中的字段<br/>
- * 提供int，boolean等常规数据类型的访问，<br/>
- * 对于非常规数据类型请自己调用获取getObject()方法后拆包<br/> 
-* @author wangtianyu 
-* @date 2016年9月22日 下午3:28:06 
-*  
-*/
-public class DataRow {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+/**
+ * @ClassName: DataRow
+ * @Description: * <b>描述：</b>数据行
+ *               <p>
+ *               <b>功能：</b>数据行的使用类
+ *               <p>
+ *               <b>用法：</b>
+ *               <p>
+ *               根据字段序号（从1开是的序号）或者字段名访问行中的字段<br/>
+ *               提供int，boolean等常规数据类型的访问，<br/>
+ *               对于非常规数据类型请自己调用获取getObject()方法后拆包<br/>
+ * @author wangtianyu
+ * @date 2016年9月22日 下午3:28:06
+ * 
+ */
+public class DataRow {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DataRow.class);
+	private int length;
+	private DataTable dataTable;
+	private Object[]values;
+	public DataRow(DataTable dataTabel,ResultSet rs){
+		this.dataTable=dataTable;
+		length=dataTable.getColumnCount();
+		values=new Object[length];
+	}
 }
--- src/main/java/com/data/database/DataTable.java
+++ src/main/java/com/data/database/DataTable.java
@@ -1,30 +1,159 @@
 package com.data.database;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
-/** 
-* @ClassName: DataTable 
-* @Description:  * <b>描述：</b> 数据库表<br/>
- * <p>
- * <b>功能：</b> 提供对表内的列和行的访问
- * <p>
- * <b>用法：</b>
+/**
+ * @ClassName: DataTable
+ * @Description: * <b>描述：</b> 数据库表<br/>
+ *               <p>
+ *               <b>功能：</b> 提供对表内的列和行的访问
+ *               <p>
+ *               <b>用法：</b>
  * 
- * 注意：对于列的访问，序号从1开始，比如访问第一行的第一列：myTable.getRow(0).getInt(1);</br>
- * 根据名称获取列序号也是一样的，获取的都是从1开始的编号；</br>
- * 这么做是为了和JDBC保持一致</br>  
-* @author wangtianyu 
-* @date 2016年9月18日 下午3:27:32 
-*  
-*/
+ *               注意：对于列的访问，序号从1开始，比如访问第一行的第一列：myTable.getRow(0).getInt(1);</br>
+ *               根据名称获取列序号也是一样的，获取的都是从1开始的编号；</br>
+ *               这么做是为了和JDBC保持一致</br>
+ * @author wangtianyu
+ * @date 2016年9月18日 下午3:27:32
+ * 
+ */
 public class DataTable {
-private static final DataColumn[]EMPTY_COLUMNS = new DataColumn[0];
-private static final Map<String,Integer>EMPTY_COLUMN_NAME_INDEX=Collections.unmodifiableMap(new HashMap<String, Integer>(1));
-private static final List<DataRow>EMPTY_ROWS=Collections.unmodifiableList(new ArrayList<DataRow>(1)); 
-private String name;
+	private static final DataColumn[] EMPTY_COLUMNS = new DataColumn[0];
+	private static final Map<String, Integer> EMPTY_COLUMN_NAME_INDEX = Collections
+			.unmodifiableMap(new HashMap<String, Integer>(1));
+	private static final List<DataRow> EMPTY_ROWS = Collections.unmodifiableList(new ArrayList<DataRow>(1));
+	private String name;
+	private DataColumn[] columns;
+	private Map<String, Integer> columnsNameIndex;
+	private List<DataRow> rows;
+
+	public DataTable(ResultSet rs) throws SQLException {
+		if (rs == null || rs.getMetaData().getColumnCount() == 0) {
+			this.name = "";
+			this.columns = EMPTY_COLUMNS;
+			this.columnsNameIndex = EMPTY_COLUMN_NAME_INDEX;
+			this.rows = EMPTY_ROWS;
+		} else {
+			ResultSetMetaData meta = rs.getMetaData();
+			rows=new ArrayList<>();
+			name=meta.getTableName(1);
+			columns=new DataColumn[meta.getColumnCount()];
+			columnsNameIndex=new HashMap<String,Integer>(meta.getColumnCount());
+			for(int i=1;i<=meta.getColumnCount();i++){
+				DataColumn dataColumn=new DataColumn(this,meta.getColumnLabel(i),meta.getColumnType(i));
+				columns[i-1]=dataColumn;
+				columnsNameIndex.put(meta.getColumnLabel(i), i);
+			}
+		}
+		while (rs.next()) {
+			rows.add(new DataRow(this, rs));
+		}
+	}
+	/**
+	 * 获取表名
+	 * @return String
+	 */
+	public String getName(){
+		return name;
+	}
+	
+	/**
+	 * 设置表名
+	 * @param name
+	 */
+	public void setName(String name){
+		this.name=name;
+	}
+	/**
+	 * 获取列数量
+	 * @return int
+	 */
+	public int getColumnCount() {
+		return columns.length;
+
+	}
+	/**
+	 * 获取指定列
+	 * @param columnIndex 列序号，第一列为1，第二列为2
+	 * @return DataColumn
+	 */
+	public DataColumn getColumn(int columnIndex) {
+		return columns[columnIndex-1];
+	}
+	
+	/**
+	 * 返回列集合
+	 * @return DataColumn数组
+	 */
+	public DataColumn[] getColumns(){
+		return columns;
+	}
+	/**
+	 * 返回是否含有指定列
+	 * @param columnName 列名
+	 * @return boolean
+	 */
+	public boolean isContainColumn(String columnName) {
+		return columnsNameIndex.containsKey(columnName);
+	}
+
+	/**
+	 * 根据名字获取列
+	 * @param columnName
+	 * @return	如果没有找到，返回null
+	 */
+	public DataColumn getColumn(String columnName) {
+		if(columnsNameIndex.containsKey(columnName))
+			return columns[columnsNameIndex.get(columnName)];
+		else
+			return null;
+	}
+	
+	/**
+	 * 获取列的序号
+	 * @param columnName
+	 * @return Integer
+	 */
+	public Integer getColumnIndex(String columnName){
+		return columnsNameIndex.get(columnName);
+	}
+
+	/**
+	 * 返回记录的数量
+	 * @return int
+	 */
+	public int getRowCount() {
+		return rows.size();
+	}
+
+	/**
+	 * 获取指定记录
+	 * @param rowIndex
+	 * @return DataRow
+	 */
+	public DataRow getRow(int rowIndex) {
+		return rows.get(rowIndex);
+	}
+
+	/**
+	 * 获取所有记录
+	 * @return ArrayList
+	 */
+	public List<DataRow> getRows() {
+		return rows;
+	}
+	
+	/**
+	 * @return 如果有结果集，返回true；否则返回false
+	 */
+	public boolean hasResultSet() {
+		return this.columns != EMPTY_COLUMNS; 
+	}
 }

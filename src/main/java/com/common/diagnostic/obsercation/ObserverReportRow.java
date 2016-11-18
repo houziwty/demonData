fe8214634ca5ac5bc63f7db823f0fc27a16eda6b
王天宇 <wangtianyu@zhongwenweilai.com>
2016-11-18 22:35:58 +0800

    完成data

--- src/main/java/com/common/diagnostic/obsercation/ObserverReportRow.java
+++ src/main/java/com/common/diagnostic/obsercation/ObserverReportRow.java
@@ -1,5 +1,41 @@
 package com.common.diagnostic.obsercation;
 
 public class ObserverReportRow {
+	private int p;
+	private String instance;
+	private String[] data;
 
+	public String[] getData() {
+		return data;
+	}
+
+	public String getInstanceName() {
+		return instance;
+	}
+
+	public ObserverReportRow(int size, String instance) {
+		p = 0;
+		data = new String[size];
+		this.instance = instance;
+	}
+
+	protected ObserverReportRow(String[] data, String instance) {
+		this.data = data;
+		this.instance = instance;
+	}
+
+	/**
+	 * 
+	 * 输出一项的值
+	 * 
+	 * @param value
+	 */
+	public void output(String value) {
+		if (value == null)
+			value = "";
+		else if (value.equalsIgnoreCase("NaN"))
+			value = "0";
+		data[p] = value;
+		p++;
+	}
 }

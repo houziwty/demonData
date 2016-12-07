09f4ea1a91722c1146e8e6cb4a39e0afb91957f0
wangtianyu <houziwty@126.com>
2016-12-07 20:48:36 +0800

    增加限流

--- src/main/java/com/common/threading/FixedObservableExecutor.java
+++ src/main/java/com/common/threading/FixedObservableExecutor.java
@@ -5,7 +5,6 @@ import java.util.concurrent.Executor;
 public class FixedObservableExecutor extends ObservableExecutor {
 
 	public FixedObservableExecutor(String name, Executor innerExecutor, int limit, int size) {
-		// TODO Auto-generated constructor stub
 	}
 
 }
---
+++ src/main/java/com/data/limiter/Limiter.java
@@ -0,0 +1,30 @@
+package com.data.limiter;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+///限流器
+public class Limiter {
+	private int maxCount;
+	private AtomicInteger counter;
+	private int maxCountPerSecond;
+	private AtomicInteger counterPerSecond;
+
+	/**
+	 * 构造函数
+	 * 
+	 * @param maxCount
+	 *            最大限流数量-10分钟内积压直
+	 * @param maxCountPerSecond
+	 *            每秒最大限流数量
+	 */
+	public Limiter(int maxCount, int maxCountPerSecond) {
+		this.maxCount = maxCount;
+		this.maxCountPerSecond = maxCountPerSecond;
+		counter = new AtomicInteger(0);
+		counterPerSecond = new AtomicInteger(0);
+	}
+	public void increment(){
+		counter.incrementAndGet();
+		counterPerSecond.incrementAndGet();
+	}
+}

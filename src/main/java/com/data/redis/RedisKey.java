25830f0c3a2cc542758d721010515fa1a5950247
wangtianyu <houziwty@126.com>
2016-12-19 20:40:22 +0800

    redis 分布式

--- src/main/java/com/data/redis/RedisKey.java
+++ src/main/java/com/data/redis/RedisKey.java
@@ -3,4 +3,15 @@ package com.data.redis;
 import java.io.Serializable;
 
 public class RedisKey implements Serializable {
+
+	/** 
+	* @Fields serialVersionUID : TODO
+	*/
+	private static final long serialVersionUID = 1L;
+	
+	//每个业务不同的
+	private String family;
+	
+	
+	private String key;
 }

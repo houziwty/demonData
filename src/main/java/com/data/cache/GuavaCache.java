0355e821ccdbc820bc520178c5091a363262581b
Demon <houziwty@126.com>
2016-12-21 18:02:21 +0800

    guava缓存

--- src/main/java/com/data/cache/GuavaCache.java
+++ src/main/java/com/data/cache/GuavaCache.java
@@ -6,22 +6,27 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
-public class GuavaCache<K,V> {
-	
-	
-	public   V put(K key,V  value){
-		LoadingCache<K, V>graphs=
-		CacheBuilder.newBuilder().maximumSize(1000).expireAfterWrite(10, TimeUnit.MINUTES)
-		.build(new CacheLoader<K, V>(){
-			@Override
-			public V load(K key) throws Exception {
-				return  (V) key;
-			}
-			
-		});
-		return value;
-	}
-	
-	
+public class GuavaCache<K, V> {
+
+    LoadingCache<K, V> graphs;
+    GuavaCache(K key,V value){
+        graphs =CacheBuilder.newBuilder().maximumSize(1000).expireAfterWrite(10, TimeUnit.MINUTES)
+                .build(new CacheLoader<K, V>() {
+                    @Override
+                    public V load(K key) throws Exception {
+                        return (V) value;
+                    }
+                });
+    }
+
+    public void put(K key, V value) {
+
+        graphs.put(key,value);
+    }
+
+    public V get(K key, V value) {
+        return value;
+    }
+
 
 }
---
+++ src/test/java/com/data/GuavaTest.java
@@ -0,0 +1,27 @@
+package com.data;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+/**
+ * Created by Haoweilai on 2016/12/21.
+ */
+public class GuavaTest {
+    public static void main(String []args){
+        System.out.println("begin");
+//        loadCache();
+    }
+    static  void loadCache(){
+        LoadingCache<String,String> cache= CacheBuilder.newBuilder().build(
+                new CacheLoader<String, String>() {
+                    @Override
+                    public String load(String s) throws Exception {
+                        return s;
+                    }
+                }
+        );
+System.out.println(cache.apply("test"));
+
+    }
+}

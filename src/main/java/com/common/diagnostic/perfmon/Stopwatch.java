dc206d9158d98db12fe42ed4883a69c1dcf73f76
Demon <houziwty@126.com>
2017-02-04 17:52:42 +0800

    完善stopwatch

--- src/main/java/com/common/diagnostic/perfmon/Stopwatch.java
+++ src/main/java/com/common/diagnostic/perfmon/Stopwatch.java
@@ -1,6 +1,9 @@
 package com.common.diagnostic.perfmon;
 
-/** 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
 * @ClassName: Stopwatch 
 * @Description: 自动开始的计时秒表, 精度到纳秒1E-9 
 * @author wangtianyu 
@@ -13,5 +16,20 @@ public class Stopwatch {
 		void fail(long nanos,String message);
 		void fail(long nanos,Throwable error);
 	}
-
+private long begin;
+	private AtomicBoolean isDone=new AtomicBoolean(false);
+	private Watchable watchable;
+	public Stopwatch(Watchable watchable){
+		this.watchable=watchable;
+		begin=System.nanoTime();
+	}
+	public Stopwatch(){
+		begin=System.nanoTime();
+	}
+	public void update(){
+		begin=System.nanoTime();
+	}
+	public long getBeginNanos(){
+		return begin;
+	}
 }

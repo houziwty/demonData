518aa981b88f2e635be2b84b53d7b6b5a1fd232b
DemonWang <houziwty@126.com>
2017-06-26 15:25:48 +0800

    动态数据源

--- src/main/java/com/data/datasource/DynamicDataSource.java
+++ src/main/java/com/data/datasource/DynamicDataSource.java
@@ -1,14 +1,50 @@
 package com.data.datasource;
 
+import org.apache.log4j.Logger;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+import javax.sql.DataSource;
+import java.util.Map;
 
 /**
  * Created by Demon on 2017/6/19.
  * 动态数据源
  */
 public class DynamicDataSource extends AbstractRoutingDataSource {
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    /**
+     * dataSource
+     */
+    private Map<String, DataSource> dataSource;
+
+    private DataSourceHolder dsh;
+
+    public Map<String, DataSource> getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(Map<String, DataSource> dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public DataSourceHolder getDsh() {
+        return dsh;
+    }
+
+    public void setDsh(DataSourceHolder dsh) {
+        this.dsh = dsh;
+    }
+
     @Override
     protected Object determineCurrentLookupKey() {
+//do nothing
         return null;
     }
+    @Override
+    protected  DataSource determineTargetDataSource(){
+        DataSource returnDataSource=this.dataSource.get(this.dsh.getDataSource());
+        return returnDataSource;
+    }
+
 }
